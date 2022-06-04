29ca644d4077ab2e296cd680872d4f1af0789273
irfandra <58932978+irfandra@users.noreply.github.com>
2022-06-04 23:01:17 +0700

    adding HTML and CSS

---
+++ src/main/resources/static/js/site.js
@@ -0,0 +1,67 @@
+﻿
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+var moneyConversions = document.querySelectorAll(".moneyConversion");
+for(let element of moneyConversions){
+	let convertedValue = Number(element.value).toFixed(2);
+	element.value = convertedValue;
+}
+var numberInputs = document.querySelectorAll("[type=number]");
+for(let element of numberInputs){
+	if(element.value == ""){
+		element.value = 0;
+	}
+}
+var dateInputs = document.querySelectorAll("[type=date]");
+for(let element of dateInputs){
+	if(element.getAttribute("value") != ""){
+		let dateValue = new Date(element.getAttribute("value"));
+		let formatted = dateValue.toISOString().split('T')[0];
+		element.value = formatted;
+	}
+}
+let alternateAction = document.querySelector(".alternate-action table");
+if(alternateAction != null){
+	let actionType = alternateAction.getAttribute("data-action");
+	document.querySelector(".alternate-action").setAttribute("action", actionType);
+	if(actionType === "update"){
+		document.querySelector(".readonly-id").setAttribute("readonly", "readonly");
+	}
+}
+let role = document.querySelector(".role");
+if(role != null){
+	let roleName = role.textContent;
+	let formatted = roleName.replace('[', '').replace(']', '').replace('ROLE_', '');
+	role.textContent = formatted;
+}
+let mainBody = document.querySelector(".main-body");
+let roleName = mainBody.getAttribute("data-role");
+let formatted = roleName.replace('[', '').replace(']', '').replace('ROLE_', '');
+mainBody.setAttribute("data-role", formatted);
+
