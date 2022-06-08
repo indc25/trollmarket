9f8f1c732c2f6592f756fcd532276e21fee199db
ANDREAS <andreas.oktafiann@gmail.com>
2022-06-08 14:23:00 +0700

    Bersih bersih file "ANDREAS"

--- src/main/java/com/exercise/trollmarket/servicesMVC/Complaint.java
+++
@@ -1,6 +0,0 @@
-package com.exercise.trollmarket.servicesMVC;
-
-public class Complaint {
-// HAM GW MAU COMPLAIN BOLEH?
-// KENAPA SAYA KAYA ANAK TIRI!!!!!!!!!!!!!!!!!!!!!!!!!!! SATTTTTTTTTTTTTTTTu
-}
